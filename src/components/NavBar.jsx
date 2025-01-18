601bc44ed49aecfe60d941cb3f93363da633dfbc
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2025-01-18 08:40:36 +0000

    feito a responsividade da navBar, tem que mudar a cor do hamburguerIcon caso seja necessario

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,101 @@
 import c from "../styles/NavBar.module.css";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useState } from "react";
+// import { Backdrop } from "@mui/material";
 export default function NavBar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNavBar = () => {
+    setIsActive(!isActive);
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const toggleAll = () => {
+    toggleNavBar();
+    handleToggle();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    toggleNavBar();
+  };
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
       <header className={c.header}>
         <img src={logo} alt="teste" />
         <ul className={c.navBar}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className={c.link}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/ensaios">Ensaios</Link>
+            <Link to="/ensaios" className={c.link}>
+              Ensaios
+            </Link>
           </li>
           <li>
-            <Link to="/eventos">Eventos</Link>
+            <Link to="/eventos" className={c.link}>
+              Eventos
+            </Link>
           </li>
           <li>
-            <Link to="/eventos">Eventos</Link>
+            <Link to="/eventos" className={c.link}>
+              Eventos
+            </Link>
           </li>
         </ul>
+        {/* responsive nav-bar */}
+
+        <div className={c.div_nav_responsive}>
+          <div
+            className={isActive ? `${c.icon} ${c.iconActive}` : c.icon}
+            onClick={toggleAll}
+          >
+            <div className={`${c.hamburguer} ${c.hamburguerIcon}`}></div>
+          </div>
+
+          <div
+            className={
+              isActive ? `${c.menu} ${c.menuOpen}` : `${c.menu} ${c.menuClose}`
+            }
+          >
+            <div className={c.navBar_responsive}>
+              <Link to="/" className={c.link_responsive} onClick={toggleAll}>
+                Home
+              </Link>
+              <Link
+                to="/ensaios"
+                className={c.link_responsive}
+                onClick={toggleAll}
+              >
+                Ensaios
+              </Link>
+              <Link
+                to="/eventos"
+                className={c.link_responsive}
+                onClick={toggleAll}
+              >
+                Eventos
+              </Link>
+              <Link
+                to="/contact-me"
+                className={c.link_responsive}
+                onClick={toggleAll}
+              >
+                Contact
+              </Link>
+              <div onClick={handleClose}></div>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
