59b6302dae604919ce391719d6febd4483e99732
Djavan <djavan1921@gmail.com>
2025-01-19 14:38:03 -0300

    Criando o arquivo do componente slide

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/ensaios" element={<Ensaios />} />
+            
             {/* <Route path="/favorites" element={<FavoritesPage />} /> */}
           </Routes>
         </BrowserRouter>
---
+++ src/components/SlideEnsaios.jsx
@@ -0,0 +1,10 @@
+import c from "../styles/SlideEnsaios.module.css";
+
+
+export  default function SlideEnsaios(){
+    return(
+        <>
+           <div className={c.container}>Slide Ensaios</div>
+        </>
+    )
+}
