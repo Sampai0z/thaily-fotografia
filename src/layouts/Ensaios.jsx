c667f1828a211e14ff9f9d394b610f27bb9590b7
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2025-02-02 22:47:10 +0000

    armazenando fotos na nuvem e recuperando para a parte dos ensaios

--- src/layouts/Ensaios.jsx
+++ src/layouts/Ensaios.jsx
@@ -1,3 +1,17 @@
 export default function Ensaios() {
-  return <div>Ensaios</div>;
+  const urls = [];
+
+  for (let index = 1; index <= 17; index++) {
+    urls.push(
+      `https://storage.cloud.google.com/thaily-fotografia/Marangon%20(${index}%20de%2017).jpg`
+    );
+  }
+
+  return (
+    <div>
+      {urls.map((url, index) => (
+        <img key={index} src={url} alt={`Imagem ${index + 1}`} width="30%" />
+      ))}
+    </div>
+  );
 }
