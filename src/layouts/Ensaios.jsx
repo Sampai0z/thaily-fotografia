fa86178ea87c260daad789f31e37dec95cf952ad
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2025-02-07 17:02:07 +0000

    terminado json com link para cada foto no bucket do supaBase

--- src/layouts/Ensaios.jsx
+++ src/layouts/Ensaios.jsx
@@ -1,35 +1,35 @@
 import fotos from "../assets/fotos.json"; // Importando o arquivo JSON
 
 export default function Ensaios() {
-  // https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/gomes/10.jpeg
+  // https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/pamella/49.jpeghttps://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/38.jpeg
 
-  let urls = [];
-  for (let index = 1; index <= 37; index++) {
-    let url = `https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/gabriel-morango/${index}.jpeg`;
-    urls.push(url);
-  }
-  console.log(urls);
+  // let urls = [];
+  // for (let index = 1; index <= 39; index++) {
+  //   let url = `https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/${index}.jpeg`;
+  //   urls.push(url);
+  // }
+  // console.log(urls);
+
   return (
     <div>
       <h1>Imagens</h1>
       {fotos.map((pasta, index) => (
         <div key={index}>
           <h2>{pasta.pasta}</h2>
-          {pasta.arquivos.length > 0 ? (
+          {/* {pasta.arquivos.length > 0 ? (
             <div>
               {pasta.arquivos.map((link, idx) => (
                 <img
                   key={idx}
                   src={link}
                   alt={`Imagem ${idx + 1}`}
-                  // className={c.HFAOUFGHSAOU}
                   style={{ maxWidth: "50%", margin: "10px" }}
                 />
               ))}
             </div>
           ) : (
             <p>Sem imagens disponíveis</p>
-          )}
+          )} */}
         </div>
       ))}
     </div>
