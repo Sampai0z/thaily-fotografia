d70432b7c47d8624efe818d9286a86cd18b78d0a
Djavan <djavan1921@gmail.com>
2025-02-09 12:21:49 -0300

    Estilizando os ensaios com grid Masonry

--- src/layouts/Ensaios.jsx
+++ src/layouts/Ensaios.jsx
@@ -1,35 +1,36 @@
 import fotos from "../assets/fotos.json"; // Importando o arquivo JSON
+import c from "../styles/layouts/Ensaios.module.css";
 
 export default function Ensaios() {
-  // https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/pamella/49.jpeghttps://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/38.jpeg
+  //https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/pamella/49.jpeghttps://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/38.jpeg
 
-  // let urls = [];
-  // for (let index = 1; index <= 39; index++) {
-  //   let url = `https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/${index}.jpeg`;
-  //   urls.push(url);
-  // }
-  // console.log(urls);
+  let urls = [];
+  for (let index = 1; index <= 39; index++) {
+    let url = `https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/paulino/${index}.jpeg`;
+    urls.push(url);
+  }
+  console.log(urls);
 
   return (
     <div>
       <h1>Imagens</h1>
       {fotos.map((pasta, index) => (
-        <div key={index}>
-          <h2>{pasta.pasta}</h2>
-          {/* {pasta.arquivos.length > 0 ? (
-            <div>
+        <div key={pasta.id || index} className={c.containerImagens}>  {/* Use um ID único se possível */}
+          <span className={c.nomePasta}>{pasta.pasta}</span>
+          {pasta.arquivos.length > 0 ? (
+            <div className={c.galeria}>
               {pasta.arquivos.map((link, idx) => (
                 <img
                   key={idx}
                   src={link}
-                  alt={`Imagem ${idx + 1}`}
-                  style={{ maxWidth: "50%", margin: "10px" }}
+                  alt={`Imagem ${idx + 1} de ${pasta.pasta}`}
+                  className={c.fotos}
                 />
               ))}
             </div>
           ) : (
             <p>Sem imagens disponíveis</p>
-          )} */}
+          )}
         </div>
       ))}
     </div>
