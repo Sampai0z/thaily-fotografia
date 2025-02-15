f66f8e903524d0789f15c98bfda58c691dbc3030
Djavan <djavan1921@gmail.com>
2025-02-15 12:40:07 -0300

    Finzalização da pagina Trabalhos

--- src/layouts/Trabalhos.jsx
+++ src/layouts/Trabalhos.jsx
@@ -1,45 +1,48 @@
 // import * from "react";
-// import c from "../styles/layouts/Trabalhos.module.css";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import CardActionArea from "@mui/material/CardActionArea";
 
-// import * as React from 'react';
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
 import { useNavigate } from "react-router-dom";
+import c from "../styles/layouts/Trabalhos.module.css";
+
+const images = [
+  { src: "selamento.jpg", alt: "Selamento", tipo:"Casamento", categoria: "casamento", },
+  { src: "pre_wedding.jpg", alt: "Pre wedding", tipo:"Casamento", categoria: "prewedding" },
+  { src: "cerimonia_casamento.jpg", alt: "Cerimônia de Casamento", tipo:"Casamento", categoria: "casamento", },
+  { src: "ensaio_infantil.jpg", alt: "Ensaio Infantil", tipo:"Infantil", categoria:"infantil" },
+  { src: "ensaio_masculino.jpg", alt: "Ensaio Masculino", tipo:"Pré-Missão", categoria:"gabriel" },
+  { src: "ensaio_feminino.jpg", alt: "Ensaio Feminino", tipo:"Pré-Missão", categoria:"ensaio_pamella" },
+  { src: "ensaio_masculino_2.jpg", alt: "Ensaio Masculino", tipo:"Pós-Missão", categoria:"ensaio_masculino2" },
+  { src: "ensaio_feminino_2.jpg", alt: "Ensaio Feminino", tipo:"Pós-Missão", categoria:"ensaio_gomes" },
+  { src: "aniversario.jpg", alt: "Eventos", tipo:"Aniversário", categoria: "aniversario" },
+];
 
 export default function Trabalhos() {
   const navigate = useNavigate();
 
-  const card = () => {
-    let teste = "gabriel";
+  const card = (categoria) => {
+    let teste = categoria;
     navigate(`/trabalhos/${teste}`);
     console.log("click");
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea
-        onClick={() => {
-          card();
-        }}
-      >
-        <CardMedia
-          component="img"
-          height="200"
-          image="https://ilhuuqdlugmuluraghjz.supabase.co/storage/v1/object/public/thaily-fotografia/gabriel-morango/1.jpeg"
-          alt="ensaio masc - pre missão"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Ensaio Masculino
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            PRÉ MISSÃO
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className={c.Container}>
+      <span className={c.menu}>MENU?</span>
+      <div className={c.ContainerFotos}>
+      {images.map((image, index) => (
+        <div key={index} className={c.foto} onClick={() => card(image.categoria)}>
+              <img 
+                src={image.src}
+              />
+              <span className={c.titulo}>{image.alt}</span>
+              <span className={c.categoria}>{image.tipo}</span>
+            </div>
+      ))}
+      </div>
+    </div>
   );
 }
