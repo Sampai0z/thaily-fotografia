aa5d05ef075e024b3f50df56372f471091c90a30
Djavan <djavan1921@gmail.com>
2025-02-24 09:08:16 -0300

    Voltando atrás mandando só para Trabalhos

--- src/components/SlideEnsaios.jsx
+++ src/components/SlideEnsaios.jsx
@@ -53,7 +53,7 @@ export default function SlideEnsaios() {
   }, []);
 
   function Trabalhos(caminho) {
-    window.location.href = `trabalhos/${caminho}`;
+    window.location.href = `Trabalhos`;
   }
 
   return (
